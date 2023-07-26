0740ecae823dc8169f11480a64a12ea9c4034594
Eli Pousson <eli.pousson@gmail.com>
2023-07-26 10:45:05 -0400

    feat: add replace_jam_values function

---
+++ R/replace_jam_values.R
@@ -0,0 +1,37 @@
+#' Replace placeholder jam values in an ACS data frame
+#'
+#' Currently only supports variable B25035_001 from the Median Year Structure
+#' Built table.
+#'
+#' @param data Data frame with ACS data
+#' @export
+#' @importFrom dplyr filter mutate if_else
+#' @importFrom cli cli_bullets
+replace_jam_values <- function(data) {
+  stopifnot(
+    all(has_name(data, c("estimate", "variable")))
+  )
+
+  if (nrow(
+    dplyr::filter(
+      data,
+      (variable == "B25035_001"), (estimate == 0)
+    )
+  ) > 0) {
+    cli::cli_bullets(
+      c(
+        "i" = "For the {.val Median Year Structure Built} table,
+        {.val 0} is a placeholder for an estimate of 1939 or earlier.",
+        "!" = "Replacing {.val 0} values with {.val 1939}"
+      )
+    )
+  }
+
+  dplyr::mutate(
+    data,
+    estimate = dplyr::case_when(
+      (variable == "B25035_001") & (estimate == 0) ~ 1939,
+      .default = estimate
+    )
+  )
+}
