8a1f392b52fa04d97bdb5676ccc521636796f811
Eli Pousson <eli.pousson@gmail.com>
2023-07-29 14:30:37 -0400

    feat: add ggplot2 scale functions
    Also add ggplot2 and scales to Imports

--- R/ggplot2.R
+++ R/ggplot2.R
@@ -1,15 +1,16 @@
 #' Label a ggplot2 plot and add a caption based on an ACS survey year
 #'
-#' @inheritParams acs_survey_label
-#' @keywords internal
+#' `labs_acs_survey()` uses `acs_survey_label_table()` to create a label for a
+#' ggplot2 plot passed to the caption parameter of [ggplot2::labs()].
+#'
+#' @inheritParams acs_survey_label_table
+#' @export
 labs_acs_survey <- function(...,
                             caption = NULL,
                             survey = "acs5",
                             year = 2021,
                             prefix = "Source: ",
                             table = NULL) {
-  check_installed("ggplot2")
-
   ggplot2::labs(
     ...,
     caption = c(
@@ -21,5 +22,101 @@ labs_acs_survey <- function(...,
         table = table
       )
     )
+  )
+}
+
+
+#' Scales for plotting ACS data with ggplot2
+#'
+#' @inheritParams ggplot2::scale_x_continuous
+#' @name scale_acs
+NULL
+
+#' @rdname scale_acs
+#' @name scale_x_acs_estimate
+#' @export
+scale_x_acs_estimate <- function(name = "Estimate",
+                                 ...,
+                                 labels = scales::label_comma()) {
+  ggplot2::scale_x_continuous(
+    name = name,
+    labels = labels,
+    ...
+  )
+}
+
+#' @rdname scale_acs
+#' @name scale_y_acs_percent
+#' @export
+#' @importFrom ggplot2 scale_y_continuous
+#' @importFrom scales label_percent
+scale_y_acs_percent <- function(name = "Est. % of total",
+                                ...,
+                                labels = scales::label_percent()) {
+  ggplot2::scale_y_continuous(
+    name = name,
+    labels = labels,
+    ...
+    )
+}
+
+#' @rdname scale_acs
+#' @name scale_x_acs_percent
+#' @export
+#' @importFrom ggplot2 scale_x_continuous
+scale_x_acs_percent <- function(name = "Est. % of total",
+                                ...,
+                                labels = scales::label_percent()) {
+  ggplot2::scale_x_continuous(
+    name = name,
+    labels = labels,
+    ...
+  )
+}
+
+#' @rdname scale_acs
+#' @name scale_y_acs_estimate
+#' @export
+#' @importFrom scales label_comma
+scale_y_acs_estimate <- function(name = "Estimate",
+                                 ...,
+                                 labels = scales::label_comma()) {
+  ggplot2::scale_y_continuous(
+    name = name,
+    labels = labels,
+    ...
+  )
+}
+
+
+#' @rdname scale_acs
+#' @name scale_x_acs_ts
+#' @export
+scale_x_acs_ts <- function(name = "Year",
+                           ...,
+                           breaks = NULL,
+                           survey = "acs5",
+                           year = 2021) {
+
+  ggplot2::scale_x_continuous(
+    name = name,
+    breaks = breaks %||% suppressMessages(acs_survey_ts(survey, year)),
+    ...
+  )
+}
+
+#' @rdname scale_acs
+#' @name scale_y_acs_ts
+#' @export
+scale_y_acs_ts <- function(name = "Year",
+                           ...,
+                           breaks = NULL,
+                           survey = "acs5",
+                           year = 2021) {
+
+  ggplot2::scale_y_continuous(
+    name = name,
+    breaks = breaks %||% suppressMessages(acs_survey_ts(survey, year)),
+    ...
   )
 }
