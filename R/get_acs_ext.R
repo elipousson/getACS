1e359cc5b198252cb03894674de1600b428cc983
Eli Pousson <eli.pousson@gmail.com>
2023-08-09 15:44:21 -0400

    docs: add docs for acs_table_variables

--- R/get_acs_ext.R
+++ R/get_acs_ext.R
@@ -447,8 +447,22 @@ acs_table_race_iteration <- function(table,
 #' Convert a table ID into a set of variable ID using a numeric vector for
 #' variables
 #'
+#' [acs_table_variables()] supports the creation of variable ID values based on
+#' a table ID string. The returned variable IDs use the format returned by
+#' [tidycensus::get_acs()], e.g. "{table_id}_{line_number}" where the
+#' line_number is a width 3 string prefixed by "0". If variables is `NULL`, the
+#' function calls [get_acs_metadata()] with `metadata = "column"` and returns
+#' all available variables for the table for the supplied year and survey.
+#'
+#' @param table A table ID string.
+#' @param data If data is provided and table is `NULL`, table is set based on
+#'   the unique values in the "table_id" column of data. If data contains more
+#'   than one table_id value, the function will error
+#' @param variables A numeric vector corresponding to the line number of the
+#'   variables.
+#' @inheritParams get_acs_metadata
 #' @name acs_table_variables
-#' @keywords internal
+#' @returns A character vector of variable ID values.
 #' @export
 #' @importFrom stringr str_pad
 acs_table_variables <- function(table = NULL,
@@ -467,9 +481,9 @@ acs_table_variables <- function(table = NULL,
 
   if (is.null(variables)) {
     table_metadata <- get_acs_metadata(
-      survey,
-      year,
-      "column",
+      survey = survey,
+      year = year,
+      metadata = "column",
       table = table,
       error_call = error_call,
       quiet = TRUE
