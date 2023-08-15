9ec987b0441feeb484e88a79e7cc4ddae356738a
Eli Pousson <eli.pousson@gmail.com>
2023-08-14 20:03:04 -0400

    fix: correct handling of sf output

--- R/get_acs_ts.R
+++ R/get_acs_ts.R
@@ -1,11 +1,23 @@
-#' Get multiple years, tables, and geographies of ACS data
+#' Get multiple years of ACS data for time series analysis
+#'
+#' [get_acs_ts()] is a variant on [get_acs_geographies()] that supports
+#' downloading data for multiple years in addition to multiple tables or
+#' multiple geographies. The year is appended as an additional column in the
+#' returned data frame. The intended use is to provide the latest year needed
+#' and the function will download data for all non-overlapping survey periods.
+#' For example, 2021 ACS data using the 5-year sample can be compared to 5-year
+#' data from 2016 and 2011. Not all variables can be compared across different
+#' years and caution is recommended when using ACS data for time series
+#' analysis.
 #'
 #' @inheritParams get_acs_geographies
-#' @param year If length 1, the function uses [acs_survey_ts()] to get data for
-#'   all comparable survey years back to the start of the ACS. If length is
-#'   greater than 1, return the selected years even if those years may not be
-#'   valid to compare.
+#' @param year A numeric vector of years. If length 1, the function uses
+#'   [acs_survey_ts()] to get data for all comparable survey years back to the
+#'   start of the ACS. This is the recommended approach for using
+#'   [get_acs_ts()]. If length is greater than 1, return the selected years even
+#'   if those years may not be valid to compare.
 #' @inheritParams cli_quiet
+#' @returns A data frame or sf object.
 #' @export
 #' @importFrom purrr map list_cbind list_rbind
 #' @importFrom rlang try_fetch
@@ -28,7 +40,7 @@ get_acs_ts <- function(geography,
     years <- acs_survey_ts(survey, year)
   }
 
-  acs_list <- purrr::map(
+  acs_data <- purrr::map(
     seq_along(years),
     function(i) {
       rlang::try_fetch(
@@ -58,7 +70,13 @@ get_acs_ts <- function(geography,
     }
   )
 
-  purrr::list_rbind(
-    acs_list
+  acs_data <- purrr::list_rbind(
+    acs_data
   )
+
+  if (is_true(list2(...)[["geometry"]])) {
+    acs_data <- sf::st_as_sf(acs_data)
+  }
+
+  acs_data
 }
