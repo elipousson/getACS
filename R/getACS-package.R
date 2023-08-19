62a643cddbc77ea85b0925fc64898833c300a607
Eli Pousson <eli.pousson@gmail.com>
2023-08-18 22:56:32 -0400

    refactor: import tidyselect

--- R/getACS-package.R
+++ R/getACS-package.R
@@ -3,10 +3,10 @@
 
 ## usethis namespace: start
 #' @import rlang
+#' @import tidyselect
 #' @importFrom cli cli_abort cli_warn cli_bullets cli_alert_info
 #'   cli_alert_warning
 #' @importFrom glue glue
 #' @importFrom lifecycle deprecated
-#' @importFrom tidyselect all_of any_of
 ## usethis namespace: end
 NULL
