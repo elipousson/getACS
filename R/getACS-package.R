45266a07d0e29e7f56fa6363419546daeec9a327
your name <eli.pousson@gmail.com>
2023-11-04 23:45:20 -0400

    refactor: import purrr functions

--- R/getACS-package.R
+++ R/getACS-package.R
@@ -6,6 +6,7 @@
 #' @import tidyselect
 #' @importFrom cli cli_abort cli_warn cli_bullets cli_alert_info
 #'   cli_alert_warning
+#' @importFrom purrr map list_cbind list_rbind
 #' @importFrom glue glue
 #' @importFrom lifecycle deprecated
 ## usethis namespace: end
